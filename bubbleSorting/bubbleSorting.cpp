935345f94f0a41d543f81e4c118a206448b4d4a6
Christopherical <chris_dixon1989@hotmail.com>
2021-06-09 20:34:08 +0100

    made a start

--- bubbleSorting/bubbleSorting.cpp
+++ bubbleSorting/bubbleSorting.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int* bubbleTest(int thing[]){    
+    return thing;
+};
+
 int main()
 {
     int bubbles[5] = { 2,1,4,3,5 };
-    cout << bubbles[0];
+    cout << sizeof(bubbles) / sizeof(bubbles[0]);
+    
+    
     return 0;
 }
