a68bda310475c62f80e9b600568f23a8a3c70643
Christopherical <chris_dixon1989@hotmail.com>
2021-06-16 20:38:11 +0100

    Not working yet.

--- bubbleSorting/bubbleSorting.cpp
+++ bubbleSorting/bubbleSorting.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
 using namespace std;
 
-int* bubbleTest(int thing[]){    
-    return thing;
-};
 
+void arrayFunc(int* bubbleArray, int size) {	
+
+	int count = 0;
+
+	while (true) {
+		for (int i = 0; i < size; i++) {
+			if (i == 0){
+				continue;
+			}
+			
+			count = 0;
+			int before = bubbleArray[(i - 1)];
+			int current = bubbleArray[i];
+
+			cout << "before = " << before;
+			cout << " | current = " << current << endl;
+
+			if (before > current) {
+				bubbleArray[(i - 1)] = current;
+				bubbleArray[i] = before;
+				count = 1;				
+			}			
+		}
+
+		if (count == 0) {
+			break;
+		}
+	}
+}
 int main()
 {
-    int bubbles[5] = { 2,1,4,3,5 };
-    cout << sizeof(bubbles) / sizeof(bubbles[0]);
-    
-    
-    return 0;
+	int bubbleArray[6] = { 5, 9, 10, 3, 2, 1};
+	int size = (sizeof(bubbleArray) / sizeof(bubbleArray[0]));	
+
+	arrayFunc(bubbleArray, size);
+
+	for (int i = 0; i < size; i++) {
+		cout << bubbleArray[i] << " ";
+	}
+	cout << ".";
+
+	
+
+	
+	 
+	
+
+
+	return 0;
 }
